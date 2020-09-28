c5187e2c587cacdb0cd6170326dbf4d71784cab5
sunhui <sunhuizlz@yeah.net>
2020-09-29 04:28:57 +0800

    Add Chromium pack source code

--- HubbleBrowser/NtpCtrl.cs
+++ HubbleBrowser/NtpCtrl.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/********************************************************************************
+*					Open Universe - version 1.0.0.4								*
+*********************************************************************************
+* Copyright (C) 2002-2020 by Tangram Team.   All Rights Reserved.				*
+*
+* This SOURCE CODE is governed by a BSD - style license that can be
+* found in the LICENSE file.
+*
+* CONTACT INFORMATION:
+* mailto:tangramteam@outlook.com
+* https://www.tangram.dev
+********************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
