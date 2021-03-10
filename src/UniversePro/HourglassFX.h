e589caa1bcd32f394602dc58dbd3f4b5269fb44b
sunhui <sunhuizlz@yeah.net>
2021-03-10 15:54:28 +0800

    update to Version 1.0.0.202103100048

--- src/UniversePro/HourglassFX.h
+++ src/UniversePro/HourglassFX.h
@@ -9,7 +9,7 @@
 /////////////////////////////////////////////////////////////////////////////
 //
 /********************************************************************************
- *           Web Runtime for Application - Version 1.0.0.202103090047
+ *           Web Runtime for Application - Version 1.0.0.202103100048
  ********************************************************************************
  * Copyright (C) 2002-2021 by Tangram Team.   All Rights Reserved.
  * There are Three Key Features of Webruntime:
