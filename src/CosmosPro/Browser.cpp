3899968e39735119b933657983341d078d6b39c4
sunhui <sunhuizlz@yeah.net>
2021-04-19 21:34:40 +0800

    Web Runtime for Application - Version 1.0.1.202104190064

--- src/CosmosPro/Browser.cpp
+++ src/CosmosPro/Browser.cpp
@@ -1,5 +1,5 @@
 /********************************************************************************
- *           Web Runtime for Application - Version 1.0.0.202104160062
+ *           Web Runtime for Application - Version 1.0.1.202104190064
  ********************************************************************************
  * Copyright (C) 2002-2021 by Tangram Team.   All Rights Reserved.
  * There are Three Key Features of Webruntime:
@@ -23,7 +23,7 @@
  *******************************************************************************/
  
  /********************************************************************************
- *           Web Runtime for Application - Version 1.0.0.202104160062
+ *           Web Runtime for Application - Version 1.0.1.202104190064
  ********************************************************************************
  * Copyright (C) 2002-2021 by Tangram Team.   All Rights Reserved.
  * There are Three Key Features of Webruntime:
